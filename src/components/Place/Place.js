d4f42e126d391f23aeb740a0e732580bd0f81c91
helloyesgo <zone.rz.red@gmail.com>
2021-10-21 02:34:41 +0300

    err: some places dont have pics

--- src/components/Place/Place.js
+++ src/components/Place/Place.js
@@ -10,17 +10,20 @@ export default function Place() {
 
   return (
     <div>
-      <h1>Here is the Place </h1>
-      {needed_data.data.length > 0 &&
-        needed_data.data.map((item) => (
-          <DataCards
-            name={item.name.en}
-            url={item.info_url}
-            location={item.location.address.street_address}
-            //images={item.description.images[0].url}
-          />
-        ))}
+      <h1 className='text-5xl text-center p-3 my-5'>Places</h1>
+      <div className='max-w-7xl mx-auto grid grid-cols-3 gap-y-6 gap-x-14'>
+        {needed_data.data.length > 0 &&
+          needed_data.data.map((item) => (
+            <DataCards
+              key={item.id}
+              name={item.name.en}
+              url={item.info_url}
+              location={item.location.address.street_address}
+              city={item.location.address.locality}
+              images={item.description.images[0]?.url}
+            />
+          ))}
+      </div>
     </div>
   );
 }
-
