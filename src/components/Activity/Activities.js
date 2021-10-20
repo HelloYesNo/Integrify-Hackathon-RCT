4ebeba61ae37737ceba0cc803aeb76c686db3211
Samikshya <samikshyaaryal5@gmail.com>
2021-10-20 23:25:29 +0300

    working on image

--- src/components/Activity/Activities.js
+++ src/components/Activity/Activities.js
@@ -8,6 +8,28 @@ export default function Activities() {
   if (loading) return loading;
   if (error) throw error;
 
+// return (
+//   <div>
+//     {needed_data.data.length > 0 && (
+//       needed_data.data.map((item) => (
+//         <div>
+//           <h1>{item.name.en}</h1>
+//           <p> {item.location.address.street_address}</p>
+//           <div className="Box">
+//             {item.description.images && item.description.images[0] &&
+//                 <img
+//                   src={item.description.images[0].url}
+//                   alt="pics"
+//                   style={{ width: "270px", height: "270px" }}
+//                 />
+//              }
+//           </div>
+//         </div>
+//       ))
+//     )}
+//   </div>
+// );
+
   return (
     <div>
       <h1>Here is the activity </h1>
@@ -23,3 +45,5 @@ export default function Activities() {
     </div>
   );
 }
+
+
