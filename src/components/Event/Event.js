b706720cff80b340d53107f416d6d6b26edc357b
helloyesgo <zone.rz.red@gmail.com>
2021-10-21 13:10:44 +0300

    loading time fix

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -6,12 +6,9 @@ export default function Event() {
   const { data: needed_data, error, loading } = useSelect('events');
   if (loading) return loading;
   if (error) throw error;
-
-  if (error) throw error;
-
   return (
     <div>
-      <h1 className='text-5xl text-center p-3 my-5'>Places</h1>
+      <h1 className='text-5xl text-center p-3 my-5'>Events</h1>
       <div className='max-w-7xl mx-auto grid grid-cols-3 gap-y-6 gap-x-14'>
         {needed_data.data.length > 0 &&
           needed_data.data.map((item, index) => {
