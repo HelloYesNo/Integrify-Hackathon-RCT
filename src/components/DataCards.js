664b8e68e42d409f6dc2ce5feae3e03df672adcb
helloyesgo <zone.rz.red@gmail.com>
2021-10-21 12:05:41 +0300

    final style changes

--- src/components/DataCards.js
+++ src/components/DataCards.js
@@ -44,32 +44,37 @@ export default function DataCards(data) {
   // console.log('images', images);
   return (
     <div className='rounded-2xl overflow-hidden shadow-lg'>
-      <img className='w-full' alt={name} src={images} />
-      <div className='px-6 py-4'>
-        <h1 className='self-center mb-6 text-xl'>{name}</h1>
-        <h1 className='self-center mb-6 text-xl'>{date}</h1>
-        <div>
-          <a className='self-center mb-6 text-xl' href={url}>
-            Website
+      <div className='flex flex-col justify-between h-full'>
+        <img className='w-full self-stretch' alt={name} src={images} />
+        <div className='flex flex-col justify-start px-4'>
+          <h1 className='self-center text-4xl text-center'>{name}</h1>
+          <h1 className='self-center text-xl'>{date}</h1>
+          <a
+            className='text-l self-center text-center text-gray-400 hover:text-gray-800 transition-colors duration-200'
+            href={maps}>
+            {city}, {location}
           </a>
         </div>
-        <a className='self-center mb-6 text-xl' href={maps}>
-          {location}, {city}
+
+        <a
+          className='self-center text-xl  hover:text-black hover:bg-gray-500 hover:bg-opacity-25 transition duration-500 ease-in-out rounded-xl p-3 my-4'
+          href={url}>
+          Website
         </a>
-      </div>
-      <div className=''>
-        <button
-          style={border1}
-          className='bg-green-100 mx-auto rounded p-1 w-40  ml-10 '
-          onClick={interested}>
-          interested {intNum}
-        </button>
-        <button
-          style={border2}
-          className='bg-green-300 mx-auto rounded ml-10 mt-3 p-1 w-40 mb-2'
-          onClick={going}>
-          going {goNum}
-        </button>
+        <div className='flex flex-row justify-evenly h- pb-10'>
+          <button
+            style={border1}
+            className='bg-green-100 mx-auto rounded p-1 w-32'
+            onClick={interested}>
+            interested {intNum}
+          </button>
+          <button
+            style={border2}
+            className='bg-green-300 mx-auto rounded  p-1 w-32'
+            onClick={going}>
+            going {goNum}
+          </button>
+        </div>
       </div>
     </div>
   );
